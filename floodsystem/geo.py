0622885b203135760e70d1d057ca2563326af273
ak2380 <ak2380@cam.ac.uk>
2022-01-29 16:43:25 +0000

    Added 1B to geo

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,22 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
+from floodsystem.stationdata import build_station_list
+
+stations = build_station_list()
+
+def stations_by_distance(stations, p):
+    "sort stations by increasing distance from the coordinate p"
+
+    stations_distance = []
+
+    for station in stations:
+        stations_distance += (station.name, station.town, haversine(station.coord, p))
+
+    sorted_by_key(stations_distance, 2)
+
+    return stations_distance
+
+
+print(stations_by_distance(stations, (52.2053, 0.1218)))
