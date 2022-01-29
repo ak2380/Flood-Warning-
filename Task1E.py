5f7460463e98771fdd3b3b9d854bce97acb3de70
ak2380 <ak2380@cam.ac.uk>
2022-01-29 18:31:35 +0000

    1e

--- Task1E.py
+++ Task1E.py
@@ -1,14 +1,24 @@
 from floodsystem.stationdata import build_station_list
+from collections import Counter
 
 stations = build_station_list()
 
-def rivers_by_station_number(stations, N):
+def rivers_by_station_number(stations):
 
     rivers_list = []
 
     for station in stations:
-        rivers_list += station.name
+        rivers_list.append(station.river)
 
-    return rivers_list
+    rivers_list.sort()
 
-print(rivers_by_station_number)
+    rivcount = dict(Counter(rivers_list))
+
+    rivlist = []
+
+    #for key, value in rivcount:
+     #   rivlist += (key,value)
+
+    return rivcount
+
+print(rivers_by_station_number(stations))
