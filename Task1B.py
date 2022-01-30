de0aec765fee4fbc16bb6b7b5f419805a13a7270
ak2380 <ak2380@cam.ac.uk>
2022-01-30 17:05:34 +0000

    1b

--- Task1B.py
+++ Task1B.py
@@ -1,6 +1,19 @@
-from floodsystem.geo import stations_by_distance
+from floodsystem.utils import sorted_by_key  # noqa
+from haversine import haversine, Unit
 from floodsystem.stationdata import build_station_list
 
-# Build list of stations
+def stations_by_distance(stations, p):
+    "sort stations by increasing distance from the coordinate p"
+
+    stations_distance = []
+
+    for station in stations:
+        stations_distance += (station.name, station.town, haversine(station.coord, p))
+
+    return sorted_by_key(stations_distance, 2)
+
 stations = build_station_list()
-print(stations_by_distance(stations, (52.2053, 0.1218)))
+
+p = (52.2053, 0.1218)
+
+print(stations_by_distance(stations,p))
