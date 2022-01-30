8b76a33415502694a66ab92293c1b2dfff429f49
rw683 <rw683@cam.ac.uk>
2022-01-30 21:32:29 +0000

    2F finished

--- Task1F.py
+++ Task1F.py
@@ -1,7 +1,5 @@
-#from floodsystem.station import inconsistent_typical_range_stations
-from floodsystem.station import test
+from floodsystem.station import MonitoringStation, inconsistent_typical_range_stations
 from floodsystem.stationdata import build_station_list
 stations = build_station_list()
 
-#print(inconsistent_typical_range_stations(stations))
-print(test(stations))
+print(inconsistent_typical_range_stations(stations))
