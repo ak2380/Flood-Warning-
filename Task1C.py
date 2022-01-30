9512f58532056986cb3b51249ac6747c07f05738
ak2380 <ak2380@cam.ac.uk>
2022-01-30 19:19:32 +0000

    final push

--- Task1C.py
+++ Task1C.py
@@ -3,4 +3,4 @@ from floodsystem.stationdata import build_station_list
 
 # Build list of stations
 stations = build_station_list()
-print (stations_within_radius(stations, (52.2053, 0.1218), 10))
+print(stations_within_radius(stations, (52.2053, 0.1218), 10))
