449e2b5b50e0779ccff02ebe3f000efa6b64cb2d
ak2380 <ak2380@cam.ac.uk>
2022-01-30 22:18:15 +0000

    1e

--- test_geo.py
+++ test_geo.py
@@ -3,7 +3,7 @@ from floodsystem.stationdata import build_station_list
 
 def test_1B():
 
-    stations = build_station_list
+    stations = build_station_list()
 
     assert len(stations_by_distance(stations,p=(52.2053, 0.1218))) > 0
     assert stations_by_distance(stations,p=(52.2053, 0.1218))[0][0] == 'Cambridge Jesus Lock'
@@ -11,13 +11,21 @@ def test_1B():
 
 def test_1C():
 
-    stations = build_station_list
+    stations = build_station_list()
 
-    assert stations_within_radius(stations,(52.2053, 0.1218), 10) == stations_within_radius(stations,(52.2053, 0.1218), 10).sort()
+    assert sorted(stations_within_radius(stations, (52.2053, 0.1218), 10)) == stations_within_radius(stations, (52.2053, 0.1218), 10)
+
+def test_1D():
+
+    stations
 
 def test_1E():
 
-    stations = build_station_list
+    stations = build_station_list()
 
     assert type(rivers_by_station_number(stations,N=9)) == list #testing that a list is the output
     assert rivers_by_station_number(stations,N=9)[0][0] == 'River Thames' #verifying that River Thames is the closest river
+
+test_1B()
+test_1C()
+test_1E()
