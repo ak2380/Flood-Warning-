8f4713d2631cccf440e98db8cf8decdb8a1e07bd
ak2380 <ak2380@cam.ac.uk>
2022-02-22 19:13:21 +0000

    test for 2e

--- test_plot.py
+++ test_plot.py
@@ -1,14 +1,19 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
-
-
 from floodsystem.plot import plot_water_levels
+import datetime
+from floodsystem.datafetcher import fetch_measure_levels
 
 def test_2E():
 
-    stations = build_station_list
+    stations = build_station_list()
     update_water_levels(stations)
-    
-    for station in stations:
+
+    for N in [1, 10, 20, 30]:
+        station = stations[N]
+        dt = 5
+        dates, levels = fetch_measure_levels(
+            station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_levels(station, dates, levels)
 
 
 test_2E()
