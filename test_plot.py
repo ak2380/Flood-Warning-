3efe644a211607d64d9e2a82234e779f45e8d703
rw683 <rw683@cam.ac.uk>
2022-02-24 11:38:00 +0000

    pushes test2E

--- test_plot.py
+++ test_plot.py
@@ -6,6 +6,18 @@ import datetime
 import numpy as np
 import random
 
+def test_2E():
+
+    stations = build_station_list()
+    update_water_levels(stations)
+
+    for N in [1, 10, 20, 30]:
+        station = stations[N]
+        dt = 5
+        dates, levels = fetch_measure_levels(
+            station.measure_id, dt=datetime.timedelta(days=dt))
+        plot_water_levels(station, dates, levels)
+
 
 def test_plot_water_level_with_fit():
     p = 4
@@ -21,3 +33,4 @@ def test_plot_water_level_with_fit():
         plot_water_level_with_fit(station, dates, levels, p)
 
 test_plot_water_level_with_fit()
+test_2E()
